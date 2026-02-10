ead969011627c6f9cb3ae9d75de71e4c82dba172
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-02-10 21:51:50 +0100

    add new feedback component

--- 01_lesson_01/01_cohort67el-fs/src/App.jsx
+++ 01_lesson_01/01_cohort67el-fs/src/App.jsx
@@ -1,8 +1,9 @@
 import Lesson_02 from './lessons/Lesson_02/Lesson_02.jsx';
-import Homework_02 from './homeworks/Homework_02/Homework_02.jsx';
 import Lesson_03 from './lessons/Lesson_03/Lesson_03.jsx';
 import Lesson_04 from './lessons/Lesson_04/Lesson_04.jsx';
+import Homework_02 from './homeworks/Homework_02/Homework_02.jsx';
 import Homework_03 from './homeworks/Homework_03/Homework_03.jsx';
+import Homework_04 from './homeworks/Homework_04/Homework04.jsx';
 
 function App() {
   return (
@@ -10,11 +11,12 @@ function App() {
       {/*LESSONS*/}
       {/*<Lesson_02/>*/}
       {/*<Lesson_03 />*/}
-      <Lesson_04 />
+      {/*<Lesson_04 />*/}
 
       {/*HOMEWORKS*/}
       {/*<Homework_02/>*/}
       {/*<Homework_03 />*/}
+      <Homework_04 />
     </>
   );
 }
---
+++ 01_lesson_01/01_cohort67el-fs/src/homeworks/Homework_04/Homework04.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import Feedback from '../../components/Feedback/Feedback.jsx';
+
+function Homework04() {
+  return (
+    <section className="homework04-section">
+      <Feedback />
+    </section>
+  );
+}
+
+export default Homework04;
