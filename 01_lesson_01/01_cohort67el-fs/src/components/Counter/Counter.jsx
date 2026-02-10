e71e7f6cecf12618d5401daec78f7d908cb2af18
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-02-10 20:06:39 +0100

    add new lesson

---
+++ 01_lesson_01/01_cohort67el-fs/src/components/Counter/Counter.jsx
@@ -0,0 +1,33 @@
+import Button from '../Button/Button';
+
+import './styles.css';
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const decrease = () => setCount((prevValue) => prevValue - 1);
+  const increase = () => setCount(count + 1);
+
+  const [fullName, setFullName] = useState('John Johnson');
+  const changeName = () => {
+    setFullName('Brad Pitt');
+  };
+
+  return (
+    <div className="counter_wrapper">
+      <div className="button_control">
+        <Button name="-" onClick={decrease} />
+      </div>
+      <p className="count">{count}</p>
+      <div className="button_control">
+        <Button name="+" onClick={increase} />
+      </div>
+      <p className="count">{fullName}</p>
+      <div className="button_control">
+        <Button name="Change name" onClick={changeName} />
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
